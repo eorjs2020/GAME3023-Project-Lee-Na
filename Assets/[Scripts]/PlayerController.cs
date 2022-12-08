a2a5077f76c2b51a2f78a2c11a061d2249fa34c2
Default 101 <eorjs2020@gmail.com>
2022-12-08 09:42:51 -0500

    PlayerController Update

--- Assets/[Scripts]/PlayerController.cs
+++ Assets/[Scripts]/PlayerController.cs
@@ -3,28 +3,47 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+public enum PlayerDirections
+{
+    North,
+    East,
+    South,
+    West
+}
+
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private Vector3 targetLocation;
     [SerializeField] private float speed;
+    [SerializeField] private PlayerDirections direction = PlayerDirections.South;
     private bool isWalking;
     [SerializeField] private int EncounterPercentage;
     private Animator animator;
+
     public LayerMask grass;
-    private Vector2 input;
+    public LayerMask NPC;    
     public LayerMask collisionObject;
 
+    private Vector2 input;
+    public bool isInteract;
+    [Header("Dust Particals")]
+    private ParticleSystem dustTrail;
+    public Color trailGround;
+    public Color trailGrass;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        animator = GetComponent<Animator>();
+        dustTrail = GetComponentInChildren<ParticleSystem>();
+        isInteract = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         Move();
+        SetAnimationClip();
     }
 
     private void Move()
@@ -39,15 +58,28 @@ public class PlayerController : MonoBehaviour
                 targetLocation = transform.position;
                 targetLocation.x += input.x;
                 targetLocation.y += input.y;
+                if (input.x > 0)
+                    direction = PlayerDirections.East;
+                else if (input.x < 0)
+                    direction = PlayerDirections.West;
+                else if (input.y > 0)
+                    direction = PlayerDirections.North;
+                else if (input.y < 0)
+                    direction = PlayerDirections.South;
                 if (IsWalkable(targetLocation))
                     StartCoroutine(MoveCheck(targetLocation));
             }
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            InteractNPC();
         }
 
     }
     IEnumerator MoveCheck(Vector3 targetPosition)
     {
         isWalking = true;
+        createDustTrail();
         while ((targetPosition - transform.position).sqrMagnitude > Mathf.Epsilon)
         {
             float step = speed * Time.deltaTime;
@@ -59,6 +91,40 @@ public class PlayerController : MonoBehaviour
         CheckEncounters();
     }
 
+    void InteractNPC()
+    {
+        Vector3 facingDirection = new Vector3(0, 0, 0);
+        switch (direction)
+        {
+            case PlayerDirections.East:
+                facingDirection = Vector3.right;
+                break;
+            case PlayerDirections.North:
+                facingDirection = Vector3.up;
+                break;
+            case PlayerDirections.South:
+                facingDirection = Vector3.down;
+                break;
+            case PlayerDirections.West:
+                facingDirection = Vector3.left;
+                break;
+        }
+        Vector3 interact = transform.position + facingDirection;
+        var inter = Physics2D.OverlapCircle(interact, 0.3f, NPC);
+        if (inter != null)
+        {
+            inter.GetComponent<NPC>().Interact();
+            isInteract = true;
+        }
+
+    }
+
+    private void SetAnimationClip()
+    {
+        animator.SetBool("IsMoving", isWalking);
+        animator.SetInteger("Direction", (int)direction);
+
+    }
 
     private bool IsWalkable(Vector3 targetPosition)
     {
@@ -79,5 +145,19 @@ public class PlayerController : MonoBehaviour
                 //SceneManager.LoadScene("BattleScene");
             }
         }
+    }
+
+    private void createDustTrail()
+    {
+        dustTrail.GetComponent<Renderer>().material.SetColor("_Color", trailGround);
+        if (Physics2D.OverlapCircle(transform.position, 0.2f, grass))
+        {
+            dustTrail.GetComponent<Renderer>().material.SetColor("_Color", trailGrass);
+        }
+        else
+        {
+            dustTrail.GetComponent<Renderer>().material.SetColor("_Color", trailGround);
+        }
+        dustTrail.Play();
     }
 }
